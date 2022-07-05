820bf55dfe2cd5c1a5f7171463680752c179c528
MarineRack <gabrielle.nicita@gmail.com>
2022-07-05 12:46:24 +0200

    catalogo, apparizione e scomp tenda modifica

--- Angular/Pescheria/src/app/modifica-pesce/modifica-pesce.component.ts
+++ Angular/Pescheria/src/app/modifica-pesce/modifica-pesce.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input
- } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../interfaces/Categoria';
+import { Pesce } from '../interfaces/Pesce';
 import { PesceMagazzino } from '../interfaces/PesceMagazzino';
 import { Trattamento } from '../interfaces/Trattamento';
 import { CategorieService } from '../services/categorie.service';
@@ -13,7 +13,9 @@ import { TrattamentoService } from '../services/trattamento.service';
   templateUrl: './modifica-pesce.component.html',
   styleUrls: ['./modifica-pesce.component.css']
 })
-export class ModificaPesceComponent{
+export class ModificaPesceComponent {
+
+  @Output() modificato= new EventEmitter()
 
   @Input() recordModifica={
     id:0,
@@ -22,18 +24,21 @@ export class ModificaPesceComponent{
     trattamento:"",
     prezzo:0,
     descrizione:""
-  }
+  } as PesceMagazzino
 
   formModifica:FormGroup
   categoria:Categoria[]=new Array()
   trattamento:Trattamento[]=new Array()
 
+  trattRec:Trattamento={
+    id:0,
+    trattamento:""
+  }
+
   constructor(
     private categoriaSer:CategorieService,
     private fb:FormBuilder,
-    private trattamentoSer:TrattamentoService,
-    private pesceServ:PesceService) 
-
+    private trattamentoSer:TrattamentoService) 
     {
       this.categoriaSer.listaCategorie().subscribe(
 
@@ -42,10 +47,49 @@ export class ModificaPesceComponent{
         this.trattamentoSer.listaTrattamenti().subscribe(tr=>{(this.trattamento =tr )})
       })
 
-      this.formModifica=this.fb.group({})
+      this.formModifica=this.fb.group({
+        nome:["",Validators.required],
+        categoria:["",Validators.required],
+        trattamento:["",Validators.required],
+        descrizione:["",Validators.required],
+        prezzoAlKg:[0,Validators.required],
+      })
       
     }
-    subModifica(){}
+    
+    ngOnChanges(){
+      
+
+      this.trattamentoSer.trattamento(this.recordModifica.trattamento).subscribe(tr=>{
+        this.trattRec=tr
+        console.log("cambio")
+
+        this.formModifica=this.fb.group({
+          nome:[this.recordModifica.nome,Validators.required],
+          categoria:["",Validators.required],
+          trattamento:["",Validators.required],
+          descrizione:[this.recordModifica.descrizione,Validators.required],
+          prezzoAlKg:[this.recordModifica.prezzo,Validators.required],
+        })
+      })
+    }
+
+    subModifica(){
+      /*const pesce={
+        nome:this.formModifica.value.nome,
+        categoria:this.formModifica.value.categoria,
+        trattamento:this.formModifica.value.trattamento,
+        descrizione:this.formModifica.value.descrizione,
+        prezzo:{
+          prezzoAlKg:this.formModifica.value.prezzoAlKg
+        }
+      } 
+      this.pesceServ.aggiungiPesce(pesce as Pesce)*/
+      this.modificato.emit("submit")
+    }
 }
+
+   
+
 
 
